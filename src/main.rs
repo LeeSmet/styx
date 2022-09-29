d74cc6842c8a6f456c31c610ec8dcb7eb3362f50
Lee Smet <lee.smet@hotmail.com>
2022-09-29 17:05:49 +0200

    POC: bridge 2 remote TUNs over TCP
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,93 @@
 use etherparse::{ether_type, EtherType};
-use std::error::Error;
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use std::{error::Error, sync::Arc};
+use tokio::{
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 use tokio_tun::TunBuilder;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // See if a target is set on the cmd line
+    let target = std::env::args().skip(1).next();
+    // Create a listener on all interfaces, fixed port for now.
+    let listener = TcpListener::bind("[::]:9651").await?;
     // TODO: Investigate if MQ is a better approach to get multiple handles to the same device
     // instead of splitting it later.
-    let mut iface = TunBuilder::new()
-        .name("styx")
-        .tap(false)
-        .mtu(1420)
-        .packet_info(false)
-        .up()
-        .try_build()?;
+    let iface = Arc::new(
+        TunBuilder::new()
+            .name("styx")
+            .tap(false)
+            .mtu(1420)
+            .packet_info(false)
+            .up()
+            .try_build()?,
+    );
 
-    let mut iface2 = TunBuilder::new()
-        .name("styx2")
-        .tap(false)
-        .mtu(1420)
-        .packet_info(false)
-        .up()
-        .try_build()?;
-    let (mut read1, mut write1) = tokio::io::split(iface);
-    let (mut read2, mut write2) = tokio::io::split(iface2);
-
-    // NOTE: As it turns out any attempt to work with any kind of `tokio::io::copy` or similar
-    // directly on the `Tun` or the split thereof returns an IO Error 22. Manual copy however does
-    // work.
-    tokio::spawn(async move {
-        let mut buf = [0; 65535];
-        loop {
-            let n = read1.read(&mut buf).await.unwrap();
-            write2.write(&mut buf[..n]).await.unwrap();
+    tokio::spawn({
+        let iface = iface.clone();
+        async move {
+            loop {
+                // Accept new connections.
+                let (con, _) = listener.accept().await.unwrap();
+                let (mut reader, mut writer) = con.into_split();
+                let iface_read = iface.clone();
+                let iface_write = iface.clone();
+                tokio::spawn(async move {
+                    let mut buf = [0; 65535];
+                    loop {
+                        let n = iface_read.recv(&mut buf).await.unwrap();
+                        let mut s = 0;
+                        while s < n {
+                            s += writer.write(&buf[s..n]).await.unwrap();
+                        }
+                    }
+                });
+                tokio::spawn(async move {
+                    let mut buf = [0; 65535];
+                    loop {
+                        let n = reader.read(&mut buf).await.unwrap();
+                        let mut s = 0;
+                        while s < n {
+                            s += iface_write.send(&buf[s..n]).await.unwrap();
+                        }
+                    }
+                });
+            }
         }
     });
-    tokio::spawn(async move {
-        let mut buf = [0; 65535];
-        loop {
-            let n = read2.read(&mut buf).await.unwrap();
-            write1.write(&mut buf[..n]).await.unwrap();
-        }
-    });
-    // tokio::spawn(async move { tokio::io::copy(&mut read1, &mut write2).await.unwrap() });
-    // tokio::spawn(async move { tokio::io::copy(&mut read2, &mut write1).await.unwrap() });
-    // tokio::io::copy_bidirectional(&mut iface, &mut iface2)
-    //     .await
-    //     .unwrap();
 
-    // let mut buf = [0; 1500];
-    // loop {
-    //     let n = iface.read(&mut buf).await?;
-    //     // SAFETY: The unwrap is safe as we statically slice the buffer to 2 bytes.
-    //     let flags = u16::from_be_bytes(buf[0..2].try_into().unwrap());
-    //     // SAFETY: The unwrap is safe as we statically slice the buffer to 2 bytes.
-    //     let ether_type = get_ether_type(u16::from_be_bytes(buf[2..4].try_into().unwrap())).unwrap();
-    //     println!(
-    //         "Read packet ({:?}), of size {} with flags {}",
-    //         ether_type,
-    //         n - 4,
-    //         flags
-    //     );
-    // }
+    // If we set a target, connect to it.
+    if let Some(target) = target {
+        tokio::task::spawn(async move {
+            let con = TcpStream::connect(target).await.unwrap();
+            let (mut reader, mut writer) = con.into_split();
+            let iface_read = iface.clone();
+            let iface_write = iface.clone();
+            tokio::spawn(async move {
+                let mut buf = [0; 65535];
+                loop {
+                    let n = iface_read.recv(&mut buf).await.unwrap();
+                    let mut s = 0;
+                    while s < n {
+                        s += writer.write(&buf[s..n]).await.unwrap();
+                    }
+                }
+            });
+            tokio::spawn(async move {
+                let mut buf = [0; 65535];
+                loop {
+                    let n = reader.read(&mut buf).await.unwrap();
+                    let mut s = 0;
+                    while s < n {
+                        s += iface_write.send(&buf[s..n]).await.unwrap();
+                    }
+                }
+            });
+        });
+    };
 
-    tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+    tokio::time::sleep(std::time::Duration::from_secs(60 * 60 * 24)).await;
 
     Ok(())
 }
